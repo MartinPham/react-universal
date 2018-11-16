3c12dcccaad8541fd2c22866a24528f9443685bd
Martin Pham <tuyentq2009@gmail.com>
2018-11-16 22:40:11 +0100

    no message

--- server/loader.js
+++ server/loader.js
@@ -150,67 +150,6 @@ export default (req, res) => {
 
       render(store);
       store.close();
-     
-        // store.runSaga();
-      /*
-      frontloadServerRender(() =>
-        renderToString(
-          <Loadable.Capture report={m => modules.push(m)}>
-            <Provider store={store}>
-              <StaticRouter location={req.url} context={context}>
-                <Frontload isServer={true}>
-                  <App />
-                </Frontload>
-              </StaticRouter>
-            </Provider>
-          </Loadable.Capture>
-        )
-      ).then(routeMarkup => {
-
-          console.log('done frontload')
-        if (context.url) {
-          // If context has a url property, then we need to handle a redirection in Redux Router
-          res.writeHead(302, {
-            Location: context.url
-          });
-
-          res.end();
-        } else {
-          // Otherwise, we carry on...
-
-          // Let's give ourself a function to load all our page-specific JS assets for code splitting
-          const extractAssets = (assets, chunks) =>
-            Object.keys(assets)
-              .filter(asset => chunks.indexOf(asset.replace('.js', '')) > -1)
-              .map(k => assets[k]);
-
-          // Let's format those assets into pretty <script> tags
-          const extraChunks = extractAssets(manifest, modules).map(
-            c => `<script type="text/javascript" src="/${c.replace(/^\//, '')}"></script>`
-          );
-
-          // We need to tell Helmet to compute the right meta tags, title, and such
-          const helmet = Helmet.renderStatic();
-
-          // NOTE: Disable if you desire
-          // Let's output the title, just to see SSR is working as intended
-          console.log('THE TITLE', helmet.title.toString());
-
-          // Pass all this nonsense into our HTML formatting function above
-          const html = injectHTML(htmlData, {
-            html: helmet.htmlAttributes.toString(),
-            title: helmet.title.toString(),
-            meta: helmet.meta.toString(),
-            body: routeMarkup,
-            scripts: extraChunks,
-            state: JSON.stringify(store.getState()).replace(/</g, '\\u003c')
-          });
-
-          // We have all the final HTML, let's send it to the user already!
-          res.send(html);
-        }
-      });
-      */
 
 
 
