db7eee92b86c075b3db18015885f0f2952d22167
Martin Pham <tuyentq2009@gmail.com>
2018-12-06 11:43:17 +0100

    new pho?

--- src/pages/Item/styles.js
+++ src/pages/Item/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const ItemBanner = styled.div`
   background-image: url(${props => props.image});
-  background-size: cover;
+  background-size: auto 100%;
+  background-repeat: no-repeat;
   background-position: center center;
   width: 100%;
   height: 250px;
