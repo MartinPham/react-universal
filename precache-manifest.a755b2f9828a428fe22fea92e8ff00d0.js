a3f58d7c104b42081a135879a965d0b08944c53c
Martin Pham <tuyentq2009@gmail.com>
2018-12-06 11:18:07 +0100

    Updates

---
+++ precache-manifest.a755b2f9828a428fe22fea92e8ff00d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1b54b7df948e8b5df6bc2f90248ba57",
+    "url": "/react-universal/static/media/pho.b1b54b7d.jpg"
+  },
+  {
+    "revision": "4065317209839bd0d635",
+    "url": "/react-universal/static/js/runtime~main.40653172.js"
+  },
+  {
+    "revision": "fd7b8e17d8d7594ec96d",
+    "url": "/react-universal/static/js/main.fd7b8e17.chunk.js"
+  },
+  {
+    "revision": "7114275cacfaefa41dc7",
+    "url": "/react-universal/static/js/Home.7114275c.chunk.js"
+  },
+  {
+    "revision": "ed2299eda7a6f1fa656d",
+    "url": "/react-universal/static/js/Contact.ed2299ed.chunk.js"
+  },
+  {
+    "revision": "20e83f005a81f2a7274f",
+    "url": "/react-universal/static/js/About.20e83f00.chunk.js"
+  },
+  {
+    "revision": "288e6f223a5faaef52a1",
+    "url": "/react-universal/static/js/4.288e6f22.chunk.js"
+  },
+  {
+    "revision": "fd7b8e17d8d7594ec96d",
+    "url": "/react-universal/static/css/main.3ef03bf3.chunk.css"
+  },
+  {
+    "revision": "2e7e6af1195446845bb2d7318cbc029f",
+    "url": "/react-universal/index.html"
+  }
+];
